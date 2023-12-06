4f6ae1ce7630d4bede60f411176b1e31de77da65
Sk1palong <cchs33@gmail.com>
2023-12-06 16:20:25 +0900

    Feat : ErrorCode 추가

--- src/main/java/com/example/process/entity/ErrorCode.java
+++ src/main/java/com/example/process/entity/ErrorCode.java
@@ -8,6 +8,10 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode {
 
+    POST_NOT_FOUND(HttpStatus.NOT_FOUND, "게시글이 존재하지 않습니다."),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자가 존재하지 않습니다."),
+    COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "댓글이 존재하지 않습니다."),
+    SELF_LIKE_ERROR(HttpStatus.FORBIDDEN, "본인이 작성한 게시글/댓글에는 좋아요를 누를 수 없습니다."),
     INDEX_NOT_FOUND(HttpStatus.NOT_FOUND, "인덱스가 존재하지 않습니다."),
     UNKNOWN_ERROR(HttpStatus.BAD_REQUEST, "토큰이 존재하지 않습니다."),
     WRONG_TYPE_TOKEN(HttpStatus.BAD_REQUEST, "변조된 토큰입니다."),
