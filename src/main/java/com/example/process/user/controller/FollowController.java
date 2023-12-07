5313d1b655a69238534bf97270aabe6b7d4f5696
minahYu <mina010311@naver.com>
2023-12-07 22:53:12 +0900

    fix : 팔로우 엔티티 수정

--- src/main/java/com/example/process/user/controller/FollowController.java
+++ src/main/java/com/example/process/user/controller/FollowController.java
@@ -1,6 +1,11 @@
 package com.example.process.user.controller;
 
+import com.example.process.user.security.UserDetailsImpl;
+import com.example.process.user.service.FollowService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,13 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/process")
 @RequiredArgsConstructor
 public class FollowController {
-//    private final FollowService followService;
-//
-//    @PostMapping("/follow/{username}")
-//    public void follow(
-//            @PathVariable String username,
-//            @AuthenticationPrincipal UserDetailsImpl userDetails
-//    ) {
-//        followService.follow(username, userDetails.getUser());
-//    }
+    private final FollowService followService;
+
+    @PostMapping("/follow/{username}")
+    public void follow(
+            @PathVariable String username,
+            @AuthenticationPrincipal UserDetailsImpl userDetails
+    ) {
+        followService.follow(username, userDetails.getUser());
+    }
 }
--- src/main/java/com/example/process/user/entity/Follow.java
+++ src/main/java/com/example/process/user/entity/Follow.java
@@ -1,14 +1,29 @@
 package com.example.process.user.entity;
 
-/* @Entity
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
 @Getter
 @Table(name = "follow")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Follow {
-   @Id
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @Column(name = "user_id", nullable = false)
-    private User user;
-}*/
+    @Column(name = "follower_id", nullable = false)
+    private User follower;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Column(name = "following_id", nullable = false)
+    private User following;
+
+    public Follow(User follower, User following) {
+        this.follower = follower;
+        this.following = following;
+    }
+}
