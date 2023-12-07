ca2a18995e4034c0d9b487f1d857583cabecdb0e
minahYu <mina010311@naver.com>
2023-12-07 22:55:07 +0900

    feat : 팔로우 레포지토리 추가

---
+++ src/main/java/com/example/process/user/repository/FollowRepository.java
@@ -0,0 +1,8 @@
+package com.example.process.user.repository;
+
+import com.example.process.user.entity.Follow;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FollowRepository extends JpaRepository<Follow, Long> {
+
+}
