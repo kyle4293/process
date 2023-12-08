2bf9c78f4589090917f0a261ae1032e6e596f285
tpgns4283 <ksh4283o@naver.com>
2023-12-08 12:15:08 +0900

    fix @RequestMapping process->api

--- src/main/java/com/example/process/user/UserController.java
+++ src/main/java/com/example/process/user/UserController.java
@@ -1,63 +1,63 @@
 package com.example.process.user;
 
 
-import com.example.process.security.UserDetailsImpl;
 import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Slf4j
 @Controller
-@RequiredArgsConstructor
 @RequestMapping("/api")
 public class UserController {
 
     private final UserService userService;
 
-    @GetMapping("/user/login-page")
-    public String loginPage() {
-        return "login";
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
-    @GetMapping("/user/signup")
-    public String signupPage() {
-        return "signup";
-    }
-
-    @PostMapping("/user/signup")
-    public String signup(@Valid SignupRequestDto requestDto, BindingResult bindingResult) {
+    @PostMapping("/signup")
+    public ResponseEntity<String> signup(@Valid @RequestBody SignupRequestDto requestDto, BindingResult bindingResult) {
         // Validation 예외처리
         List<FieldError> fieldErrors = bindingResult.getFieldErrors();
         if(fieldErrors.size() > 0) {
             for (FieldError fieldError : bindingResult.getFieldErrors()) {
                 log.error(fieldError.getField() + " 필드 : " + fieldError.getDefaultMessage());
             }
-            return "redirect:/api/user/signup";
+            return new ResponseEntity<>("회원가입 실패", HttpStatusCode.valueOf(400));
         }
 
         userService.signup(requestDto);
 
-        return "redirect:/api/user/login-page";
+        return new ResponseEntity<>("회원가입 성공", HttpStatus.CREATED);
     }
 
-    // 회원 관련 정보 받기
-    @GetMapping("/user-info")
-    @ResponseBody
-    public UserInfoDto getUserInfo(@AuthenticationPrincipal UserDetailsImpl userDetails) {
-        String username = userDetails.getUser().getUsername();
-        UserRoleEnum role = userDetails.getUser().getRole();
-        boolean isAdmin = (role == UserRoleEnum.ADMIN);
+    //프로필 조회
+    @GetMapping("/profile/{username}")
+    public UserResponseDto getProfile(@PathVariable String username) {
+        return userService.getProfile(username);
+    }
 
-        return new UserInfoDto(username, isAdmin);
+    //프로필 수정
+    @PutMapping("/profile/{username}")
+    public UserResponseDto updateProfile(@PathVariable String username, @RequestBody UserRequestDto requestDto) {
+        return userService.updateProfile(username, requestDto);
+    }
+
+    //비밀번호 수정
+    @PutMapping("/password/{username}")
+    public ResponseEntity<String> updatePassword(@RequestBody PasswordUpdateRequestDto passwordUpdateRequest, @PathVariable String username) {
+
+        String currentPassword = passwordUpdateRequest.getCurrentPassword();
+        String newPassword = passwordUpdateRequest.getNewPassword();
+        return userService.updatePassword(username, currentPassword, newPassword);
     }
 }
